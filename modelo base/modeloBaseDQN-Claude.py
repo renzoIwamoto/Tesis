c5cc7f4ac9b51477b1ca135516407d0ae3d69430
Renzo Iwamoto <130329342+renzoIwamoto@users.noreply.github.com>
2024-08-06 00:16:13 -0500

    env:iceHockey

--- modelo base/modeloBaseDQN-Claude.py
+++ modelo base/modeloBaseDQN-Claude.py
@@ -14,7 +14,9 @@ import datetime
 #ENV_NAME = 'BreakoutDeterministic-v4'
 #ENV_NAME = 'MsPacmanDeterministic-v4'
 #ENV_NAME = 'SpaceInvadersDeterministic-v4'
-ENV_NAME = 'PongDeterministic-v4'
+#ENV_NAME = 'PongDeterministic-v4'
+ENV_NAME = 'IceHockeyDeterministic-v4'
+
 GAME_NAME = ENV_NAME.split('-')[0]
 FRAME_STACK = 4                          # Número de frames apilados para representar el estado.
 GAMMA = 0.99                             # Factor de descuento para las recompensas futuras
