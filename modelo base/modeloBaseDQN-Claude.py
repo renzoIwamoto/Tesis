ff5a6dc214298cc5de24903858cc8119f2793fe4
Renzo Iwamoto <130329342+renzoIwamoto@users.noreply.github.com>
2024-08-06 17:38:25 -0500

    env breakout

--- modelo base/modeloBaseDQN-Claude.py
+++ modelo base/modeloBaseDQN-Claude.py
@@ -11,9 +11,9 @@ from gymnasium.wrappers import RecordVideo
 import datetime
 
 # Configuración del entorno y parámetros
-#ENV_NAME = 'BreakoutDeterministic-v4'
+ENV_NAME = 'BreakoutDeterministic-v4'
 #ENV_NAME = 'MsPacmanDeterministic-v4'
-ENV_NAME = 'SpaceInvadersDeterministic-v4'
+#ENV_NAME = 'SpaceInvadersDeterministic-v4'
 #ENV_NAME = 'PongDeterministic-v4'
 #ENV_NAME = 'IceHockeyDeterministic-v4'
 
