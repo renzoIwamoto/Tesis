cfe735cd038dfc60391c214d3de245a05362e432
Renzo Iwamoto <130329342+renzoIwamoto@users.noreply.github.com>
2024-08-20 21:46:14 -0500

    deterministic enviorment

--- modelo base/DQN/modelo-base_pytorch.py
+++ modelo base/DQN/modelo-base_pytorch.py
@@ -30,7 +30,7 @@ import json
 
 
 # Configuración del entorno y parámetros
-ENV_NAME = 'ALE/Breakout-v5'
+ENV_NAME = 'BreakoutDeterministic-v4'
 GAME_NAME = ENV_NAME.split('-')[0].replace('/', '_')  # Reemplazar '/' con '_'
 FRAME_STACK = 4
 GAMMA = 0.99
