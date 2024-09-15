06c135a9b9a015221c2d1aeb584fb03706d1f769
Renzo Iwamoto <130329342+renzoIwamoto@users.noreply.github.com>
2024-09-15 10:36:27 -0500

    LOCAL_FOLDER evaluate representation

--- transferencia/representaciones/representation_transfer.py
+++ transferencia/representaciones/representation_transfer.py
@@ -308,7 +308,7 @@ def main():
         mean_reward, std_reward = utils.evaluate_agent(env, agent, num_episodes=30, FRAME_STACK=FRAME_STACK)
         logging.info(f"Final Evaluation - Mean Reward: {mean_reward}, Std Reward: {std_reward}")
         utils.plot_training_progress(scores, avg_q_values_per_episode, losses, GAME_NAME, timestamp, LOCAL_FOLDER)
-        utils.plot_evaluation_scores(evaluation_scores, GAME_NAME, timestamp)  
+        utils.plot_evaluation_scores(evaluation_scores, GAME_NAME, timestamp, LOCAL_FOLDER)  
         agent.save(os.path.join(MODELS_FOLDER, f'dqn_model_{GAME_NAME}_final_{timestamp}.pth'))
     except Exception as e:
         logging.error(f"Error al guardar el modelo o la memoria de experiencia: {e}")
